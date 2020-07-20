c465cabac66798fc91197ec5bcb1b15250a99e09
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-07-21 00:04:41 +0300

    1.Bölüm( AppDbContext ve Product class)

---
+++ EFCoreFunctionApp/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreFunctionApp.Models
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ EFCoreFunctionApp/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreFunctionApp.Models
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
