9cedbb6f6187a0fdf2494d087a1fedc36d43d4b7
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-07-26 16:37:00 +0300

    Output Blob bindings -Resize işlemi 2

--- FunctionBlobTriggerApp/ResizeFunction.cs
+++ FunctionBlobTriggerApp/ResizeFunction.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
+using Microsoft.Azure.Storage.Blob;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -12,15 +15,26 @@ namespace FunctionBlobTriggerApp
     public static class ResizeFunction
     {
         [FunctionName("ResizeFunction")]
-        public static async Task Run([BlobTrigger("udemy-pictures/{name}", Connection = "MyAzureStorage")] Stream myBlob, string name, ILogger log, [Blob("udemy-pictures-resize/{name}", FileAccess.Write, Connection = "MyAzureStorage")] Stream outputBlobStream)
+        public static async Task Run([BlobTrigger("udemy-pictures/{name}", Connection = "MyAzureStorage")] Stream myBlob, string name, ILogger log, [Blob("udemy-pictures-resize", Connection = "MyAzureStorage")] CloudBlobContainer cloudBlobContainer)
         {
+            await cloudBlobContainer.CreateIfNotExistsAsync();
+            MemoryStream ms = new MemoryStream();
             var format = await Image.DetectFormatAsync(myBlob);
 
+            var blockBLob = cloudBlobContainer.GetBlockBlobReference($"{Guid.NewGuid()}-100.{format.FileExtensions.First()}");
+            ;
             var ResizeImage = Image.Load(myBlob);
 
             ResizeImage.Mutate(x => x.Resize(100, 100));
 
-            ResizeImage.Save(outputBlobStream, format);
+            ResizeImage.Save(ms, format);
+
+            ms.Position = 0;
+
+            blockBLob.UploadFromStream(ms);
+            ms.Close();
+            ms.Dispose();
+
             log.LogInformation($"Resim resize işlemi başarıyla gerçekleştirildi.");
         }
     }
